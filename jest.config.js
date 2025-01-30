41c3fe8e01b0f1e95cce3cddb2b3099639646eb0
arturdssla <arturdsslva@gmail.com>
2025-01-29 22:53:59 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export const roots = ['<rootDir>/src'];
+export const collectCoverageFrom = ['<rootDir>/src/**/*.ts'];
+export const testEnvironment = 'node';
+export const transform = {
+    '.+\\.ts$': 'ts-jest'
+};
+export const moduleNameMapper = {
+    '@/(.*)': '<rootDir>/src/$1'
+};
---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+it('Sould sum', () => {
+  const a = 1
+  const b = 2
+  const sum = a + b
+
+  expect(sum).toBe(3)
+})
