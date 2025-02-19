2519f514e3a11371454143ed939662a754c50d8c
arturdsslva <arturdsslva@gmail.com>
2025-02-19 09:03:59 -0300

    refactor: simplify User entity by removing TypeORM decorators

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,45 +1,16 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm'
 import { ERole, ERank } from '@/core/enums'
 import { IUser } from '@/data/models/'
 
-@Entity('users')
 export class User implements IUser {
-  @PrimaryGeneratedColumn('uuid')
   _id!: string
-
-  @Column('varchar')
   name!: string
-
-  @Column('varchar')
   email!: string
-
-  @Column('varchar')
   password!: string
-
-  @Column('enum', { enum: ERole })
   role!: ERole
-
-  @Column('enum', { enum: ERank })
   rank!: ERank
-
-  @Column('boolean')
   active!: boolean
-
-  @Column('varchar', { nullable: true })
-  phone?: string | undefined
-
-  @Column('varchar', { nullable: true })
-  avatar?: string | undefined
-
-  @CreateDateColumn()
+  phone?: string
+  avatar?: string
   created_at!: Date
-
-  @UpdateDateColumn()
   updated_at!: Date
 }
