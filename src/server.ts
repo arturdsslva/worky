b739c9215d559d2da021f0006feca6cf3c78ccaa
arturdsslva <arturdsslva@gmail.com>
2025-02-18 21:23:02 -0300

    feat: update database connection initialization to use AppDataSource

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express'
 import type { Request, Response } from 'express'
 import { env } from 'config/'
-import { dataSource } from 'data/datasources/db'
+import { AppDataSource } from '@/data/datasources/ormconfig'
 
-dataSource
-  .initialize()
+AppDataSource.initialize()
   .then(() => {
     console.log('Database connected!')
 
